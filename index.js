6feb04fc236aee312fb0125ece50518e657aa28c
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 21:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ const logDriversByHometown = function(drivers, hometown) {
 }
 
 const driversByRevenue = function(drivers) {
-  return drivers.slice().sort(function())
+  return drivers.slice().sort(function(driverOne, driverTwo) {
+    
+  })
 }
