8f63a196794dfedda1d02a352e16c0279c8cdba8
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 21:35:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const logDriverNames = function(drivers) {
 
 const logDriversByHometown = function(drivers, hometown) {
   drivers.forEach(function(driver) {
-    if (driver.hometown )
+    if (driver.hometown === hometown)
   })
 }
